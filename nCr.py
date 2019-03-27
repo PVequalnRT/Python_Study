d0b1d595fd73224d327743da3c0b9e509ec3f93b
PVequalnRT <usetino913@gmail.com>
2019-03-27 13:34:41 +0900

    nCr

--- nCr.py
+++ nCr.py
@@ -1,4 +1,12 @@
-import math
+#팩토리얼 구현
+def factorial(num):
+    i = num
+    result = 1
+    while i > 0:
+        result *= i
+        i -= 1
+    return result
+
 
 #사용자에게 nCr 입력 받음
 n = int(input("총 개수를 입력해주세요 : "))
@@ -7,5 +15,5 @@ r = int(input("뽑을 개수를 입력해주세요 :"))
 if r>n :
     print("n은 r보다 크거나 같아야 합니다.")
 else :
-    result = int(math.factorial(n)/(math.factorial(r)*math.factorial(n-r)))
+    result = int(factorial(n)/(factorial(r)*factorial(n-r)))
     print(result)
