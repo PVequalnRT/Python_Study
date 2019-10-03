7326ac901815afb1586e94ee779376c7d6d66d4b
unknown <usetino913@gmail.com>
2019-10-03 17:43:23 +0900

    complete binary.py

--- Algorithm/Search/binary.py
+++ Algorithm/Search/binary.py
@@ -1,5 +1,14 @@
 def search(target, someList):
+    start_index, end_index = 0, len(someList)-1
 
+    while start_index <= end_index:
+        i = (start_index + end_index) // 2
+        if someList[i] == target:
+            return i
+        elif someList[i] > target:
+            end_index = i - 1
+        else:
+            start_index = i + 1
     return None
 
 print(search(2,[2,3,5,7,11]))
