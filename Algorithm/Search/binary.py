e696f3598fe9847e3ff2e90ed6d3a1759d7a10f1
unknown <usetino913@gmail.com>
2019-10-03 17:32:06 +0900

    modify binary.py

---
+++ Algorithm/Search/binary.py
@@ -0,0 +1,9 @@
+def search(target, someList):
+
+    return None
+
+print(search(2,[2,3,5,7,11]))
+print(search(0,[2,3,5,7,11]))
+print(search(5,[2,3,5,7,11]))
+print(search(3,[2,3,5,7,11]))
+print(search(11,[2,3,5,7,11]))
